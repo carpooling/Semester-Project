f168dd619c904d97b5dc1ec9301702a80e1523ce
carpooling <kalith.kumasaru@my.ndsu.edu>
2012-09-11 17:53:39 -0700

    updated database with references
    Ankit

--- carpooling.sql
+++ carpooling.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 3.3.9
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 06, 2012 at 02:07 AM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Generation Time: Sep 12, 2012 at 02:51 AM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -17,7 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `car_pooling`
+-- Database: `carpooling`
 --
 
 -- --------------------------------------------------------
@@ -37,6 +36,11 @@ CREATE TABLE IF NOT EXISTS `address` (
   PRIMARY KEY (`addressId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `address`
+--
+
+
 -- --------------------------------------------------------
 
 --
@@ -55,6 +59,11 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   KEY `vinNumber` (`vinNumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `reservation`
+--
+
+
 -- --------------------------------------------------------
 
 --
@@ -70,6 +79,11 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   KEY `userId` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `reviews`
+--
+
+
 -- --------------------------------------------------------
 
 --
@@ -82,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `firstName` varchar(20) NOT NULL,
   `lastName` varchar(20) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `ssn` int(12) NOT NULL,
+  `ssn` varchar(12) NOT NULL,
   `homeAddress` varchar(20) NOT NULL,
   `workAddress` varchar(20) NOT NULL,
   `contactNumber` varchar(12) NOT NULL,
@@ -92,6 +106,13 @@ CREATE TABLE IF NOT EXISTS `user` (
   `carName` varchar(20) NOT NULL,
   PRIMARY KEY (`userId`,`fbId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userId`, `fbId`, `firstName`, `lastName`, `password`, `ssn`, `homeAddress`, `workAddress`, `contactNumber`, `conctactMethod`, `emailAddress`, `gender`, `carName`) VALUES
+('0853221', 'akumar@email.com', 'ankit', 'kumar', 'behenchod', '2147483647', 'Niskanen', 'wennsoft', '7015522096', 'p', 'ankit@email.com', 'M', 'camry');
 
 -- --------------------------------------------------------
 
@@ -111,6 +132,13 @@ CREATE TABLE IF NOT EXISTS `vehicle` (
   PRIMARY KEY (`vinNumber`),
   KEY `userId` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+INSERT INTO `vehicle` (`vinNumber`, `model`, `make`, `color`, `year`, `type`, `userId`, `vehicleNickName`) VALUES
+('0892', 'camry', 'toyota', 'red', 1992, 'sedan', '0853221', 'dikra');
 
 --
 -- Constraints for dumped tables
@@ -134,7 +162,3 @@ ALTER TABLE `reviews`
 --
 ALTER TABLE `vehicle`
   ADD CONSTRAINT `vehicle_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
