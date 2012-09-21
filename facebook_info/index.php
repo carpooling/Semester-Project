0cd92b9c8f43314f518ef1f9a5a2b93ab6bb4690
carpooling <kalith.kumasaru@my.ndsu.edu>
2012-09-20 17:20:42 -0700

    --added files

---
+++ facebook_info/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Getting info</title>
+</head>
+
+<body>
+<iframe src="https://www.facebook.com/plugins/registration?
+             client_id=113869198637480&
+             redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fecho%2F&
+             fields=name,birthday,gender,location,email"
+        scrolling="auto"
+        frameborder="no"
+        style="border:none"
+        allowTransparency="true"
+        width="100%"
+        height="330">
+</iframe>
+</body>
+</html>
