f4c1ecddbc056ec6ecb4d2106fac4a923254c02e
carpooling <kalith.kumasaru@my.ndsu.edu>
2012-09-20 17:35:34 -0700

    --readstuff.php

---
+++ facebook_info/readstuff.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<?php
+define('FACEBOOK_APP_ID', '356974527720638');
+define('FACEBOOK_SECRET', '029652c6f03024fffb111c1e0312ae4d');
+
+function parse_signed_request($signed_request, $secret) {
+  list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+
+  // decode the data
+  $sig = base64_url_decode($encoded_sig);
+  $data = json_decode(base64_url_decode($payload), true);
+
+  if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+    error_log('Unknown algorithm. Expected HMAC-SHA256');
+    return null;
+  }
+
+  // check sig
+  $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+  if ($sig !== $expected_sig) {
+    error_log('Bad Signed JSON signature!');
+    return null;
+  }
+
+  return $data;
+}
+
+function base64_url_decode($input) {
+    return base64_decode(strtr($input, '-_', '+/'));
+}
+
+if ($_REQUEST) {
+  echo '<p>signed_request contents:</p>';
+  $response = parse_signed_request($_REQUEST['signed_request'], 
+                                   FACEBOOK_SECRET);
+  echo '<pre>';
+  print_r($response);
+  echo '</pre>';
+} else {
+  echo '$_REQUEST is empty';
+}
+?>
+<body>
+</body>
+</html>
